6be63650f1e01da1049f44b59fe5c256daf1c2df
Johan LEPINAY <johan.lepinay@yanipel.com>
2021-12-18 00:53:58 +0400

    Cleaner project

--- config/db.js
+++ config/db.js
@@ -2,14 +2,12 @@ const mongoose = require("mongoose");
 
 mongoose
     .connect(
-        "mongodb+srv://" + process.env.DB_USER_PASS + "@cluster0.wx7x5.mongodb.net/social-network",
-        //mongodb + srv://lepic974:<password>@cluster0.wx7x5.mongodb.net/test
-        //"mongodb+srv//process.env.DB_USER_PASS@cluster0.wx7x5.mongodb.net/social-network",
+        "mongodb+srv://" + process.env.DB_USER_PASS + "@cluster0.wx7x5.mongodb.net/mem-project",
         {
             useNewUrlParser: true,
             useUnifiedTopology: true,
-            useCreateIndex: true,
-            useFindAndModify: false,
+            //useCreateIndex: true,
+            // useFindAndModify: false,
         }
     )
     .then(() => console.log("Connected to MangoDB"))
